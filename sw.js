d1e75bcaf7213bb5c345d788f97bf22c3994d04c
yaofuzhang <4163067.zhang@163.com>
2026-08-05 20:15:20 +0800

    Initial release: 英语征途 RPG 学习 APP

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='english-rpg-v1';const URLS=['/','/index.html','/manifest.json'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS)).catch(()=>{}))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)).catch(()=>caches.match('/index.html')))})
